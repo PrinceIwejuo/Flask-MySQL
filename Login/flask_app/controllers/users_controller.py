83074943d7288d16c6c8bcd6c46bdaefdaf031d6
PrinceIwejuo <Princeiwejuo@gmail.com>
2022-02-24 20:05:07 -0500

    LoginProject

---
+++ Login/flask_app/controllers/users_controller.py
@@ -0,0 +1,68 @@
+from flask_app import app
+
+from flask import render_template, redirect, request, session, flash
+
+from flask_app.models.user import User
+
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+@app.route('/users/register', methods=['POST'])
+def register_user():
+
+    if not User.validate_new_user(request.form):
+        print("validation failed")
+        return redirect('/')
+
+    else:
+        print("validation worked")
+        data = {
+            'first_name': request.form['first_name'],
+            'last_name': request.form['last_name'],
+            'email': request.form['email'],
+            'password': bcrypt.generate_password_hash(request.form['password'])
+        }
+        print(data)
+        User.create_new_user(data )
+        flash("User is now registered; log in with that account")
+        return redirect('/')
+
+@app.route('/users/login', methods=['POST'])
+def login_user():
+    
+    user = User.get_user_by_email(request.form)
+
+    if not user:
+        flash("This email does not exist.")
+        return redirect('/')
+
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Password is incorrect")
+        return redirect('/')
+
+    session['user_id'] = user.id
+    session['user_email'] = user.email
+ 
+    return redirect('/success')
+
+@app.route('/success')
+def success():
+
+    if not 'user_id' in session:
+        flash ("You have to log in to see this page, sorry bud :/ ")
+        return redirect('/')
+
+    return render_template('success.html')
+
+@app.route('/users/logout')
+def logout():
+    session.clear()
+    flash("You've logged out, see you next time!")
+    return redirect('/')
---
+++ Login/flask_app/models/user.py
@@ -0,0 +1,72 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+
+from flask import flash
+
+import re
+
+class User():
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.first_name = data['first_name']
+        self.last_name = data['last_name']
+        self.email = data['email']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+    @classmethod
+    def create_new_user(cls, data):
+        query = "INSERT INTO users (first_name, last_name, email, password) VALUES (%(first_name)s, %(last_name)s, %(email)s, %(password)s);"
+
+        result =connectToMySQL("Login_schema").query_db(query, data)
+
+        return result
+
+    @classmethod
+    def get_user_by_email(cls, data):
+
+        query = "SELECT * FROM users WHERE email = %(email)s"
+
+        results = connectToMySQL("Login_schema").query_db(query, data)
+
+        if len(results) == 0:
+            return False
+        
+        else:
+            return User(results[0])
+
+        return results 
+
+    @staticmethod
+    def validate_new_user(data):
+        is_valid = True
+
+        email_regex = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') 
+
+        if User.get_user_by_email(data):
+            is_valid = False 
+            flash("Email should be unique")
+
+        if len(data['first_name']) < 3 or len(data['first_name']) > 45:
+            is_valid = False
+            flash("First name should be 3 to 45 characters long")
+
+        if len(data['last_name']) < 3 or len(data['last_name']) > 45:
+            is_valid = False
+            flash("Last name should be 3 to 45 characters long")
+
+        if not email_regex.match(data['email']):
+            is_valid = False
+            flash("Email adress is not formatted correctly.")
+        
+        if len(data['password']) < 8:
+            is_valid = False
+            flash("Passwrod should be atleast eight characters long")
+        
+        if not data['password'] == data['confirm_password']: 
+            is_valid = False
+            flash("Password and confirm password does not match")
+        
+        return is_valid
+
